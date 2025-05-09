b51769340b9442444cb837bf6bed841873aa7a9f
Kevin Neyer <kevin.neyer@swingu.com>
2025-05-09 12:45:39 -0400

    update repo to have typewriter component

---
+++ src/typewriter.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const Typewriter = (props) => {
+  const { flag } = props;
+  const [currentFlag, setcurrentFlag] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex < flag.length) {
+      const timeout = setTimeout(() => {
+        setcurrentFlag((letter) => letter + flag[currentIndex]);
+        setCurrentIndex((index) => index + 1);
+      }, 500);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, flag]);
+
+  return (
+    <ul>
+      <li>{currentFlag}</li>
+    </ul>
+  );
+};
+
+export default Typewriter;
